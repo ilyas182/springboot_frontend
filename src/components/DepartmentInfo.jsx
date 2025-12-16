168751335f96e23087880068f367721942b46503
Ilyas Effendi <ilyas2104@gmail.com>
2025-12-16 15:25:56 +0800

    render index for employees

--- src/components/DepartmentInfo.jsx
+++ src/components/DepartmentInfo.jsx
@@ -1,34 +1,45 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 export default function DepartmentInfo() {
 
     const { departmentId } = useParams();
-    const [page, setPage] = useState(1);
+    const [searchParams, setSearchParams] = useSearchParams('page=1');
+    const [page, setPage] = useState(Number(searchParams.get('page')) || 1);
     const [departmentData, setDepartmentData] = useState(null);
 
     useEffect(() => {
-        // Fetch department info based on departmentId
         const fetchDepartmentInfo = async () => {
-            // Placeholder for fetch logic
             const response = await axios.get(`http://localhost:9090/employees/dept/${departmentId}?page=${page}`);
             setDepartmentData(response.data);
         }
         fetchDepartmentInfo();
     }, [departmentId, page]);
+
+    const handleBackBtn = () => {
+        if (page > 1) {
+            setPage(page - 1);
+            setSearchParams({ page: page - 1 });
+        }
+    }
+    
+    const handleNextBtn = () => {
+        setPage(page + 1);
+        setSearchParams({ page: page + 1 });
+    }
     return (
         <div className="p-8">
             <h2 className="text-2xl font-bold mb-4">Department Information</h2>
             {departmentData && departmentData.map((employee, index) => (
                 <div key={index} className="mb-2">
-                    <p className="text-lg">{index+1}. {employee.first_name} {employee.last_name} - {employee.emp_no}</p>
+                    <p className="text-lg">{(page - 1) * 20 + index + 1}. {employee.first_name} {employee.last_name} - {employee.emp_no}</p>
                 </div>
             ))}
             <div>
-            <a href="#" onClick={() => setPage(page - 1)} className="text-blue-500 hover:underline mr-4">Back</a>
-            <span style={{margin: '16px'}} className="text-2xl">Page {page}</span>
-            <a href="#" onClick={() => setPage(page + 1)} className="text-blue-500 hover:underline ml-4">Next</a>
+            <a href="#" onClick={handleBackBtn} className="text-blue-500 hover:underline mr-4">Back</a>
+            <span className="text-2xl mx-4">Page {page}</span>
+            <a href="#" onClick={handleNextBtn} className="text-blue-500 hover:underline ml-4">Next</a>
             </div>
         </div>
     )
