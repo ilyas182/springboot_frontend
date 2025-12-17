0d2ee3d490bb3e3f8aacf9c6cc33525ab5373e2c
Ilyas Effendi <ilyas2104@gmail.com>
2025-12-17 20:48:54 +0800

    add table for deparment page

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -24,13 +24,22 @@ export default function Departments() {
     return (
         <div className="p-8">
             <h2 className="text-2xl font-bold mb-4">Departments</h2>
-            <ul>
-                {departments.map((department, key) => (
-                    <div key={key} className="mb-2">
-                        <Link key={department.dept_no} to={`/departments/${department.dept_no}`} >{department.dept_no} - {department.dept_name}</Link>
-                    </div>
-                ))}
-            </ul>
+            <table className='text-sm text-left rtl:text-right text-body'>
+            <thead className='text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default'>
+            <tr>
+                <th scope='col' className="px-6 py-3 font-medium">Department name</th>
+                <th scope='col' className="px-6 py-3 font-medium">Department number</th>
+            </tr>
+            </thead>
+            <tbody>
+                {departments.map((department, index) => (
+                <tr className='bg-neutral-primary border-b border-default' key={index}>
+                <th scope="row" className='px-6 py-4 font-medium text-heading whitespace-nowrap'><Link to={`/departments/${department.dept_no}`}>{department.dept_name}</Link></th>
+                <td className='px-6 py-4'><Link to={`/departments/${department.dept_no}`}>{department.dept_no}</Link></td>
+            </tr>
+            ))}
+            </tbody>
+            </table>
         </div>
     )
 }
